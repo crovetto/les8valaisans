4cff802b22f6dd9727c1489b3c04ba5bb377ab97
crovetto <gianna.crovetto@epfl.ch>
2026-09-05 21:07:48 +0200

    app sur phone

---
+++ service-worker.js
@@ -0,0 +1,77 @@
+const CACHE_NAME =
+  'les8valaisans-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+
+self.addEventListener(
+  'install',
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(APP_FILES)
+        )
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  'activate',
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys =>
+          Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          )
+        )
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  'fetch',
+  event => {
+
+    event.respondWith(
+
+      fetch(event.request)
+        .catch(() =>
+          caches.match(
+            event.request
+          )
+        )
+
+    );
+
+  }
+);
